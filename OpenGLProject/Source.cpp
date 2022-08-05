f9074fabb6f793d290fef47290392801d9bf15cd
jfprgin <jprgin00@fesb.hr>
2022-08-05 14:41:47 +0200

    First Commit

---
+++ OpenGLProject/Source.cpp
@@ -0,0 +1,22 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+int main(int argc, char **argv)
+{
+	glfwInit();
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, " Hello OpenGL ", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+	glewInit();
+
+	while (!glfwWindowShouldClose(window)) {
+		// render our scene
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwTerminate();
+
+
+	return 0;
+}
